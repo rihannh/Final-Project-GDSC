a648d7e71982589f69201a7a4f8a4c97a5a2d4db
Rihan Naufaldihanif <rihannaufal22@gmail.com>
2024-07-01 20:27:13 +0700

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { FaLinkedin } from "react-icons/fa";
+function App() {
+  return (
+    <>
+      <h1 className='font-bold text-3xl'>Rihan <FaLinkedin /></h1>
+    </>
+  )
+}
+
+export default App
